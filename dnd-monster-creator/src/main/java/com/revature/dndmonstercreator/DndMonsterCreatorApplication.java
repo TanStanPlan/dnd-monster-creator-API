22301d8f6f17ba7e6f60adc98f9a91779a2bf606
Tanner Stanley <35375292+TanStanPlan@users.noreply.github.com>
2022-10-09 19:34:47 -0400

    Add files via upload

---
+++ dnd-monster-creator/src/main/java/com/revature/dndmonstercreator/DndMonsterCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.revature.dndmonstercreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndMonsterCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndMonsterCreatorApplication.class, args);
+	}
+
+}
