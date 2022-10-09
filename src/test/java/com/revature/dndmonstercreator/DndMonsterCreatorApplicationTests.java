1c44d1951462684cfb0f63439a36d2bd4777ae57
Tanner Stanley <35375292+TanStanPlan@users.noreply.github.com>
2022-10-09 19:36:18 -0400

    Add files via upload

---
+++ src/test/java/com/revature/dndmonstercreator/DndMonsterCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.dndmonstercreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndMonsterCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
